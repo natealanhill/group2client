81fcd43fe644d15e9eba4205c6b870dcf8d5fa4c
artempenn <agervets197@gmail.com>
2021-02-23 20:32:15 -0500

    added the routing and a sample display can be deleted later

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,7 +1,119 @@
+import Container from '@material-ui/core/Container';
+
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Grid from '@material-ui/core/Grid';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link';
+
+
 const Dashboard = () => {
+    const useStyles = makeStyles((theme) => ({
+        icon: {
+          marginRight: theme.spacing(2),
+        },
+        heroContent: {
+          backgroundColor: theme.palette.background.paper,
+          padding: theme.spacing(8, 0, 6),
+        },
+        heroButtons: {
+          marginTop: theme.spacing(4),
+        },
+        cardGrid: {
+          paddingTop: theme.spacing(8),
+          paddingBottom: theme.spacing(8),
+        },
+        card: {
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+        },
+        cardMedia: {
+          paddingTop: '56.25%', // 16:9
+        },
+        cardContent: {
+          flexGrow: 1,
+        },
+        footer: {
+          backgroundColor: theme.palette.background.paper,
+          padding: theme.spacing(6),
+        },
+      }));
+    
+
+    const topStyles = {
+        background: "rgba(0, 0, 0, 0.65)",
+    }
+
+    const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    const classes = useStyles();
+
     return (
         <div>
-        
+            <CssBaseline />
+      <AppBar position="relative">
+
+ 
+
+      </AppBar>
+      <main>
+        {/* Hero unit */}
+        <div className={classes.heroContent}
+            style={topStyles}>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h4" align="center" gutterBottom>
+              Welcome, User! 
+            </Typography>
+
+            <Button variant="contained">
+                Add Beer
+            </Button>
+
+          </Container>
+        </div>
+        <Container className={classes.cardGrid} maxWidth="md">
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {cards.map((card) => (
+              <Grid item key={card} xs={12} sm={6} md={4}>
+                <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.cardMedia}
+                    image="https://source.unsplash.com/random"
+                    title="Image title"
+                  />
+                  <CardContent className={classes.cardContent}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      Heading
+                    </Typography>
+                    <Typography>
+                      This is a media card. You can use this section to describe the content.
+                    </Typography>
+                  </CardContent>
+                  <CardActions>
+                    <Button size="small" color="primary">
+                      Edit
+                    </Button>
+                    <Button size="small" color="primary">
+                      Delete
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+      {/* Footer */}
+
         </div>
     )
 }
