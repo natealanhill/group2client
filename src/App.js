5d96af0533e25c67dad671c32162fcac05f43def
Nate Hill <nate.alan.hill@gmail.com>
2021-03-02 17:27:44 -0600

    Fixed last name

--- src/App.js
+++ src/App.js
@@ -25,21 +25,9 @@ function App() {
             </Route>
 
           </Switch>
-<<<<<<< HEAD
-
-
-        <img src={logo} className="App-logo" alt="logo" style={{ width: '15%', height: '15%' }} />
-        <p>
-          {/* <code style={welcomeStyles}>Welcome to your Beer Wingman.</code> */}
-          {/* <p style={{marginTop: '2px', marginBottom: '3px ', fontSize: '15px'}}> Create your own custom BeerJournal to rate, describe, and remember the beers you taste or drink. </p> */}
-        </p>
-        <GetStarted />
-        <BeerCard />
-=======
        
        
         
->>>>>>> 00d97450c00d2f893716485c2a2595d15f20f169
         {/* <Button variant="contained">Get Started</Button> */}
 
       </header>
--- src/components/LoginComponents/Register.js
+++ src/components/LoginComponents/Register.js
@@ -65,7 +65,7 @@ const Register = () => {
               autoComplete="lastName"
               autoFocus
               onChange={(e)=> setLastName(e.target.value)} 
-              value={firstName}
+              value={lastName}
             />
       
               <TextField
