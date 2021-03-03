907df8bcd70ccadc0fbb32f2919b1f14627e225e
Nate Hill <nate.alan.hill@gmail.com>
2021-03-02 19:34:50 -0600

    create view card

---
+++ src/components/Cards/ViewBeer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+// import Input from '@material-ui/core/Input';
+import { CardContent } from '@material-ui/core';
+// import TextField from '@material-ui/core/TextField';
+import PropTypes from 'prop-types';      //------------------------
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+import Dialog from '@material-ui/core/Dialog';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Radio from '@material-ui/core/Radio';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import './BeerCard.css';
+import TextField from '@material-ui/core/TextField';
+import SimpleRating from './beerRating';
+import Box from '@material-ui/core/Box';
+
+// pull information from the database per user *
+// beer Name
+// location
+// rating
+// notes
+
+
+
+
+
+const viewBeer ({})
