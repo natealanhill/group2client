4c8e32fda0c0dff491621b0a25313a1d971fdc7d
artempenn <agervets197@gmail.com>
2021-03-03 20:23:58 -0500

    passed token as prop to ViewBeer.js to get beers to display

--- src/App.js
+++ src/App.js
@@ -1,82 +1,69 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 
-import './App.css';
-import GetStarted from './components/GetStarted'
-  
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import Auth from './components/LoginComponents/Auth'
+import "./App.css";
+import GetStarted from "./components/GetStarted";
 
-import Dashboard from './components/Dashboard'
-import ViewBeer from './components/Cards/ViewBeer'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Auth from "./components/LoginComponents/Auth";
+
+import Dashboard from "./components/Dashboard";
+import ViewBeer from "./components/Cards/ViewBeer";
 
 function App() {
-
- 
-  const [sessionToken, setSessionToken] = useState('');
+  const [sessionToken, setSessionToken] = useState("");
 
   useEffect(() => {
-    if(localStorage.getItem('token')){
-      setSessionToken(localStorage.getItem('token'));
+    if (localStorage.getItem("token")) {
+      setSessionToken(localStorage.getItem("token"));
     }
-  }, [])
+  }, []);
 
   const updateToken = (newToken) => {
-    localStorage.setItem('token', newToken);
+    localStorage.setItem("token", newToken);
     setSessionToken(newToken);
     console.log(sessionToken);
-  }
+  };
 
   const clearToken = () => {
     localStorage.clear();
-    setSessionToken('');
-  }
+    setSessionToken("");
+  };
 
   const protectedViews = () => {
-    return sessionToken === localStorage.getItem('token') ? (
+    return sessionToken === localStorage.getItem("token") ? (
       <Router>
-      <Switch>
-      <Route exact path="/">
-         <Auth updateToken={updateToken} />
-  
-  
-      </Route>
-      {/* <Button variant="contained">Get Started</Button> */}
-  
-      <Route exact path="/dashboard">
-        <Dashboard />
-      </Route>
-  
-  
-      <Route exact path="/Cards/viewBeer">
-        <ViewBeer /> 
-      </Route>      
-      
-    </Switch>
-    </Router>
+        <Switch>
+          <Route exact path="/">
+          <Dashboard /> 
+          </Route>
+          {/* <Button variant="contained">Get Started</Button> */}
+
+          <Route exact path="/dashboard">
+              <Auth updateToken={updateToken} />
+          </Route>
+
+          <Route exact path="/Cards/viewBeer">
+            <ViewBeer token={sessionToken} />
+          </Route>
+        </Switch>
+      </Router>
     ) : (
       <GetStarted updateToken={updateToken} />
     );
-  }
+  };
 
   return (
-   
     <div className="App">
       <header className="App-header">
+        {protectedViews()}
 
+        {/* Add all entires       */}
+        {/* <ViewAllBeers /> */}
 
-          {protectedViews()}
-          
-          {/* Add all entires       */}
-          {/* <ViewAllBeers /> */}
-        
         {/* <Button variant="contained">Get Started</Button> */}
-
       </header>
     </div>
   );
 }
 
 export default App;
-
-
-  
