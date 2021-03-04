ee1a1cc38f4912bd2c7d56dbe9d024800f1a6950
Britt Braswell <brittani.braswell@gmail.com>
2021-03-03 23:58:20 -0500

    removed const firstname and lastname from Register.js

--- src/components/LoginComponents/Register.js
+++ src/components/LoginComponents/Register.js
@@ -12,8 +12,7 @@ const Register = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [firstName, setFirstName] = useState('');
-    const [lastName, setLastName] = useState('');
+ 
     const register = async () => {
       let userObj = {
         user: {
