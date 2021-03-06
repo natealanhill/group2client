6d968646568ad26bc649ac4020decb728573b348
Nate Hill <nate.alan.hill@gmail.com>
2021-03-06 10:38:40 -0600

    Saturday Help

--- src/components/Cards/DeleteBeerCard.js
+++ src/components/Cards/DeleteBeerCard.js
@@ -9,7 +9,6 @@ import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import {Link} from "react-router-dom";
-// import BWRating from './BWRating.txt';
 
 const useStyles = makeStyles({
     root: {
